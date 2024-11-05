f9883453702d91ec680f9813defc879b6f13c0a4
Tony Nhampulo <geral@tonynhampulo.com>
2024-11-05 11:03:45 +0200

    Initial App setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-center w-full text-2xl font-bold text-gray-600">
+        Barbershop App
+      </h1>
+    </>
+  );
+}
+
+export default App;
